96ac8227d74039dad1ef1fd18d14bca1bbd92f62
李潇 <xiao.li@caringnurse.cn>
2021-12-02 18:43:15 +0800

    完成登录

--- test_platform/test_platform/urls.py
+++ test_platform/test_platform/urls.py
@@ -19,6 +19,10 @@ from user_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('accounts/login/', views.index),
     path("login_action/", views.login_action),
+    path("logout/", views.logout),
+    path("project_manage/", views.project_manage),
+
 ]
 
--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import decorators
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect, response
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
@@ -23,8 +26,26 @@ def login_action(request):
             print(user)
             print(type(user))
             if user is not None:
-                auth.login(request,user) #验证登录
-                return render(request,"project_manage.html")
+                auth.login(request,user) #记录用户登录状态
+                request.session['user1']=username
+                response = HttpResponseRedirect("/project_manage/")
+                #response.set_cookie('user1',username,3600)
+                return response
+             
             else:
                 return render(request, "index.html", {"error": "用户名或者密码错误"})
 
+
+@login_required #判断用户是否登录
+def project_manage(request):
+    username =request.session.get('user1','')
+    #username =request.COOKIES.get('user1','')
+    return render(request, "project_manage.html",{'user':username})
+
+def logout(request):
+    auth.logout(request)
+    response =HttpResponseRedirect('/')
+    return response
+
+
+    
